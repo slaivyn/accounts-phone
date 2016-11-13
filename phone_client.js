6dc99508f6259383aad46c8eb201c81e9b592e88
Sylvain Duchesne <pro@sylvainduchesne.com>
2016-11-13 16:47:01 +0100

    if installed use accountsp-assword to login with username/password or email/password

--- phone_client.js
+++ phone_client.js
@@ -56,6 +56,26 @@ Meteor.loginWithPhoneAndPassword = function (selector, password, callback) {
     });
 };
 
+if(Package['accounts-password']) {
+    const _loginWithPassword = Meteor.loginWithPassword
+    Meteor.loginWithPassword = function(selector, password, callback) {
+        console.log("login", selector, password)
+        let username
+        if(typeof selector === 'string' && selector.indexOf('@') === -1) {
+            username = selector
+        } else {
+            if(selector.username) {
+                username = selector.username
+            }
+        }
+        if(username && /^\+33/.test(username)) {
+            Meteor.loginWithPhoneAndPassword({phone: username}, password, callback)
+        } else {
+            _loginWithPassword(selector, password, callback)
+        }
+    }
+}
+
 Accounts._hashPassword = function (password) {
     return {
         digest   : SHA256(password),
@@ -184,4 +204,3 @@ Accounts.isPhoneVerified = function () {
     var me = Meteor.user();
     return !!(me && me.phone && me.phone.verified);
 };
-
